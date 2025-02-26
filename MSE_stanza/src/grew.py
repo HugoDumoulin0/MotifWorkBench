16d0b9f49d953326ef835cc12c4e17f948f22a87
Your Name <you@example.com>
2025-02-26 16:40:41 +0100

    version de grew.py pour test des conllu pouvant être lancé de manière autonome

--- MSE_stanza/src/grew.py
+++ MSE_stanza/src/grew.py
@@ -39,13 +39,13 @@ def index(treebank_path, req, param):
     return index
 
 
-# grewpy.set_config("sud") # ud or basic 
+grewpy.set_config("sud") # ud or basic 
 
 # path="/Users/hugodumoulin/Desktop/ArchivU/Travail/motifs/grewpy-tutorial/SUD_English-PUD/"
 # treebank_path="/Users/hugodumoulin/Desktop/ArchivU/Travail/motifs/grewpy-tutorial/SUD_English-PUD/en_pud-sud-test.conllu"
 
 # path="/Users/hugodumoulin/Desktop/ArchivU/Travail/motifs/out_s/MSE_stanza/Data/Textes_tagged_stanza/ISP"
-# treebank_path="//Users/hugodumoulin/Desktop/ArchivU/Travail/motifs/out_s/MSE_stanza/Data/Textes_tagged_stanza/ISP/ISP.conllu"
+conllu="./Data/Textes_tagged_stanza/ARSCAN/ARSCAN.conllu"
 
 # corpus = Corpus(treebank_path)
 # print(type(corpus))
@@ -53,9 +53,11 @@ def index(treebank_path, req, param):
 
 # req1 = Request("pattern { X1 [upos=NOUN, Gender=Masc] ; X2 [Number=Sing, upos=ADJ] ; X1<X2}")
 # req1 = Request('pattern { X1 [Number=Sing,upos=DET,lemma="le",Definite=Def,PronType=Art] ; X2 [Number=Sing] ; X1<X2}')
-# req1 = Request('pattern { X1 [upos=DET]; X2 [upos=ADJ]; X3 [upos=NOUN]; X1 < X2; X2 < X3}')
+req1 = Request('pattern { X1 [upos=DET]; X2 [upos=ADJ]; X3 [upos=NOUN]; X1 < X2; X2 < X3}')
 # req1 = Request('pattern { X1 [upos=PRON, Person="3", PronType=Prs] ; X2 [Person="3"]; X1 < X2}')
 
+index(conllu, req1, param="lemma")
+print(index)
 # liste_match = corpus.search(req1, deco=True)
 
 # for i in index(treebank_path, req1, "lemma"):
