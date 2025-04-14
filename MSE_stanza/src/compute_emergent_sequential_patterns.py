5b20bff0169dcedbc500d43be121113c74568c05
Your Name <hugo.dumoulin@hotmail.fr>
2025-04-14 11:47:11 +0200

    mise en forme

--- MSE_stanza/src/compute_emergent_sequential_patterns.py
+++ MSE_stanza/src/compute_emergent_sequential_patterns.py
@@ -64,7 +64,6 @@ def compute_GR(type_1, type_2):
 
         nbr_seq_1 = get_nbr_seq("./Data/DMT4_files/DMT4_{}_files_sorted.txt".format(type_1))
         nbr_seq_2 = get_nbr_seq("./Data/DMT4_files/DMT4_{}_files_sorted.txt".format(type_2))
-        # 21-02-2025 : nbr_seq_ = number of sentences of the corpus (from conl_dmt4.transform_data, l. 131)
 
 
         # lexic_int_str = formate_patterns.load_lexique() ### Modification du script de Jade qui présente un problème ici ####
@@ -75,23 +74,20 @@ def compute_GR(type_1, type_2):
         index_p = 0
         for p in dict_1:
             freq_1 = dict_1.get(p)
-                #21-02-2025: this is absolute freq (number of sentences comprising motif)
             if p in dict_2:
                 freq_2 = dict_2.get(p)
                 gr = (freq_1 / nbr_seq_1) / (freq_2 / nbr_seq_2)
-                    #21-02-2025: freq_1 / nbr_seq_1) = relative freq, i.e., what is called
-                    #"frequence" in Mekki PhD (2022: p. 95)
                 dict_gr[index_p] = [formate_patterns.from_str_to_list(p),
                                     formate_patterns.from_int_to_str(p, lexic_int_str),
                                     gr,
-                                    freq_1, #21-02-2025: it is absolute freq that is written in .tsv
+                                    freq_1,
                                     freq_2]
             else:
                 gr = freq_1
                 dict_gr[index_p] = [formate_patterns.from_str_to_list(p),
                                     formate_patterns.from_int_to_str(p, lexic_int_str),
                                     gr,
-                                    (freq_1 / nbr_seq_1), #21-02-2025: it is relative freq that is written in .tsv
+                                    (freq_1 / nbr_seq_1),
                                     0]
             index_p+=1
         file_out = "./Patterns_results/Emergent/{}_00_{}_{}.pk".format(mins,
@@ -100,6 +96,3 @@ def compute_GR(type_1, type_2):
         save_pickles_results(dict_gr, file_out)
         from_dict_to_df(file_out)
     return True
-
-#21-02-2025: There is quite a problem there: GR and freq have different definitions depending on wether
-#the motif appears in both corpora or not.
