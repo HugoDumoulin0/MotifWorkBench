5c8226a72abf125a8cb32b139f78262037e0472a
Your Name <hugo.dumoulin@hotmail.fr>
2025-04-16 14:21:45 +0200

    déplacemet de concat dans tools, ajout de nouvelles fonctions pour essayer d'aider interpret_association.py

--- MSE_stanza/src/tools.py
+++ MSE_stanza/src/tools.py
@@ -9,7 +9,8 @@ Created on Tue Feb 25 14:44:41 2025
 import numpy as np
 from scipy.stats import hypergeom
 import pickle as pk
-
+import os
+from conllu import parse_incr
 
 def indice_specificite(k,f,t,T):
     rv = hypergeom(T, f, t)
@@ -42,3 +43,32 @@ def load_pickles(title_file):
 def get_nbr_seq(dmt4_files):
         with open("{}".format(dmt4_files), 'r', encoding="utf-8") as dmt4 :
             return len([line for line in dmt4.readlines() if "seqId" in line])
+        
+def concat_multiple_conll(path, files_list, output_file):
+    with open(f'{output_file}', 'w', encoding='utf-8') as out:
+        for file in files_list:
+            if file!=".DS_Store": 
+                with open(f'{path}{file}', 'r', encoding='utf-8') as f:
+                    out.write(f.read())
+                    out.write('\n')
+                    os.remove(f'{path}{file}')
+  
+def unique_sentences_from_stanza(folder_path):
+    all_sentences = []
+    for filename in sorted(os.listdir(folder_path)):
+        if filename!=".DS_Store":
+            filepath = os.path.join(folder_path+"/"+filename, filename)
+            prefix = os.path.splitext(filename)[0]  # nom du fichier comme préfixe
+            with open(filepath, "r", encoding="utf-8") as f:
+                for i, sentence in enumerate(parse_incr(f), 1):
+                    sentence.metadata["sent_id"] = f"{prefix}_{i}"
+                    all_sentences.append(sentence)
+    return all_sentences
+
+def concat_preserve_ids(path_input, output_file):
+    sentences = unique_sentences_from_stanza(path_input)
+    with open(output_file, "w", encoding="utf-8") as out_f:
+        for sentence in sentences:
+            out_f.write(sentence.serialize())
+            out_f.write("\n")
+    print("Merging done")
