6864f0388b2fc29121494692eeeb2bb29cda77f5
Your Name <hugo.dumoulin@hotmail.fr>
2025-04-20 21:09:59 +0200

    function-like writing

--- MSE_stanza/src/conllu2vrt.py
+++ MSE_stanza/src/conllu2vrt.py
@@ -10,49 +10,48 @@ from conllu import parse_incr
 import sys
 import os
 
-path_stanza="./Data/Textes_tagged_stanza/"
-path_vrt="./Data/textesVRT/"
-
-if not os.path.exists(path_vrt):
-    os.mkdir(path_vrt)
-    
-output_file = f"{path_vrt}merged.vrt"
-
-
 def format_value(value):
     # Si c'est un dict (ex: misc), on le formate comme "key1=val1|key2=val2"
     if isinstance(value, dict):
         return "|".join(f"{k}={v}" for k, v in value.items())
     return str(value).replace(" ", "_") 
 
-with open(output_file, "w", encoding="utf-8") as f_out:
-    for file in os.listdir(path_stanza):
-        if file==".DS_Store": continue
-        f_out.write(f'<text id="{file}">\n')
-        input_file=f"{path_stanza}{file}/{file}"
-        with open(input_file, "r", encoding="utf-8") as f_in:
-            sentence_id = 0            
-            for tokenlist in parse_incr(f_in):
-                sentence_id += 1
-                f_out.write(f'<s id="{sentence_id}">\n')
-                for token in tokenlist:
-                    # On ignore les multi-word tokens (ID de type tuple)
-                    if isinstance(token.get("id", None), tuple):
-                        continue
-                    values = [
-                    token.get("id", "_"),
-                    token.get("form", "_"),
-                    token.get("lemma", "_"),
-                    token.get("upostag", "_"),
-                    token.get("xpostag", "_"),
-                    token.get("feats", "_"),
-                    token.get("head", "_"),
-                    token.get("deprel", "_"),
-                    token.get("deps", "_"),
-                    token.get("misc", "_")
-                    ]
-                    formatted = [format_value(v) for v in values]
-                    f_out.write("\t".join(formatted) + "\n")
-                f_out.write("</s>\n")
-        f_out.write("</text>\n")
+def transform(path_stanza, path_vrt):
+    if not os.path.exists(path_vrt):
+        os.mkdir(path_vrt)
+        print("Transforming Conllu to VRT...")
+        output_file = f"{path_vrt}merged.vrt"
+        with open(output_file, "w", encoding="utf-8") as f_out:
+            for file in os.listdir(path_stanza):
+                if file==".DS_Store": continue
+                f_out.write(f'<text id="{file}">\n')
+                input_file=f"{path_stanza}{file}/{file}"
+                with open(input_file, "r", encoding="utf-8") as f_in:
+                    sentence_id = 0            
+                    for tokenlist in parse_incr(f_in):
+                        sentence_id += 1
+                        f_out.write(f'<s id="{sentence_id}">\n')
+                        for token in tokenlist:
+                            # On ignore les multi-word tokens (ID de type tuple)
+                            if isinstance(token.get("id", None), tuple):
+                                continue
+                            values = [
+                            token.get("id", "_"),
+                            token.get("form", "_"),
+                            token.get("lemma", "_"),
+                            token.get("upostag", "_"),
+                            token.get("xpostag", "_"),
+                            token.get("feats", "_"),
+                            token.get("head", "_"),
+                            token.get("deprel", "_"),
+                            token.get("deps", "_"),
+                            token.get("misc", "_")
+                            ]
+                            formatted = [format_value(v) for v in values]
+                            f_out.write("\t".join(formatted) + "\n")
+                        f_out.write("</s>\n")
+                f_out.write("</text>\n")
+    else:
+        print("VRT files already exists")
+
 
