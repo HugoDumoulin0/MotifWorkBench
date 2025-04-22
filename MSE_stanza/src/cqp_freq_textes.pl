d610fcc80f60bf4eb0e60eb0f2b602df180e58bb
Your Name <hugo.dumoulin@hotmail.fr>
2025-04-22 22:40:16 +0200

    Create cqp_freq_textes.pl

---
+++ MSE_stanza/src/cqp_freq_textes.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+use CWB::CQP;
+use IPC::Open2;
+
+# Chemin vers le registre
+my $registry = './Data/cwb-corpus/registry';
+my $corpus   = 'MERGED';
+my $query    = $ARGV[0];
+my $query_text = 'group pattern match text_id';
+
+# Lancement du binaire CQP en mode silencieux (-e)
+my $cmd = "cqp -r '$registry' -e";
+my ($out, $in);
+
+my $pid = open2($out, $in, $cmd) or die "Impossible de lancer CQP";
+
+# On envoie la requête CQP
+print $in "$corpus;\n";
+print $in "$query;\n";
+print $in "$query_text;\n";
+print $in "exit;\n";
+
+# Lire et afficher les résultats de la requête
+print "Résultats pour 'laboratoire' :\n";
+while (my $line = <$out>) {
+    print $line;  # Affiche chaque ligne de la sortie
+}
