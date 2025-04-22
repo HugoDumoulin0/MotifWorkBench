7b75699ad739a4ac2a86cd0b3d148e8fbaf1dba5
Your Name <hugo.dumoulin@hotmail.fr>
2025-04-22 22:34:51 +0200

    modifying input with args

--- MSE_stanza/src/AFC.R
+++ MSE_stanza/src/AFC.R
@@ -18,28 +18,31 @@ ipak(packages)
 default_folder <- "./Patterns_results/Specifs_noZero"
 print(default_folder)
 
-input_suffix <- "_AFC_R_df"
+#input_suffix <- "_AFC_R_df"
 
-files <- list.files(default_folder, full.name = TRUE)
-matching_files <- files[grep(paste0(input_suffix, ".tsv", "$"), files)]
-print(matching_files)
+#files <- list.files(default_folder, full.name = TRUE)
+#matching_files <- files[grep(paste0(input_suffix, ".tsv", "$"), files)]
+#print(matching_files)
 
-dir.create("./Patterns_results/R")
+args <- commandArgs(trailingOnly = TRUE)
+file = args[1]
+path = args[2]
 
 
-for (file in matching_files) {
-    base_name <- basename(file)
-    input_file <- file
-    df <- read.csv(input_file, sep="\t", row.names = 1)
-    var_name <- sub("\\.tsv$", "", input_file)
-    
-    rep_name <- sub("./Patterns_results/Specifs_noZero/", "", var_name)
-    rep_name <- sub("_R_df", "", rep_name)
-    dir.create(glue("./Patterns_results/R/{rep_name}"))
 
-	var_name <- glue("./Patterns_results/R/{rep_name}/")
+base_name <- basename(file)
+input_file <- file
+df <- read.csv(input_file, sep="\t", row.names = 1)
+#var_name <- sub("\\.tsv$", "", input_file)
+
+rep_name=path
+#rep_name <- sub("./Patterns_results/Specifs_noZero/", "", var_name)
+#rep_name <- sub("_df", "", rep_name)
+#dir.create(glue("./Patterns_results/R/{rep_name}"))
+
+#var_name <- glue("./Patterns_results/R/{rep_name}/")
 	#var_name <- sub("./Patterns_results/Specifs_noZero/25_AFC_R_df", glue("./Patterns_results/R/{rep_name}/"), var_name)
-	print(var_name)
+#print(var_name)
 
 
 
@@ -116,7 +119,7 @@ AFC = CA(df, graph=F)
 #-----------------------------------
 
 #Plot degree of variance explanation per axis
-bmp(filename=paste(var_name, "screeplot.bmp", sep=""),
+bmp(filename=paste(rep_name, "screeplot.bmp", sep=""),
 	width=1536, height=1536, res=200)
 plot_obj <- fviz_screeplot(AFC,
 	addlabels = TRUE) +
@@ -131,7 +134,7 @@ dev.off()
 	#Get the 10 heaviest contributing rows
 	contrib_10_rows <- head(row$contrib, 10)
 	write.table(contrib_10_rows,
-		file = paste(var_name, "contrib_10_patterns.txt", sep=""),
+		file = paste(rep_name, "contrib_10_patterns.txt", sep=""),
 		sep = "\t",
         row.names = TRUE,
 		col.names = NA)
@@ -142,13 +145,13 @@ dev.off()
 	#Get the 10 heaviest contributing cols
 	contrib_10_cols <- head(col$contrib, 10)
 	write.table(contrib_10_cols,
-		file = paste(var_name, "contrib_10_texts.txt", sep=""),
+		file = paste(rep_name, "contrib_10_texts.txt", sep=""),
 		sep = "\t",
         row.names = TRUE,
 		col.names = NA)
 
 	#Plot it
-	bmp(filename=paste(var_name, "contrib_dim_1.bmp", sep=""),
+	bmp(filename=paste(rep_name, "contrib_dim_1.bmp", sep=""),
 		width=2048, height=2048, res=200)
 	plot_obj <- fviz_contrib(AFC,
 		choice ="col",
@@ -156,7 +159,7 @@ dev.off()
 	print(plot_obj)
 	dev.off()
 
-	bmp(filename=paste(var_name, "contrib_dim_2.bmp", sep=""),
+	bmp(filename=paste(rep_name, "contrib_dim_2.bmp", sep=""),
 		width=2048, height=2048, res=200)
 	plot_obj <- fviz_contrib(AFC,
 		choice ="col",
@@ -172,45 +175,45 @@ dev.off()
 # source("accuracy.R")
 # # var_per_axis(AFC)
 
-bmp(filename=paste(var_name, "txt_contrib10.bmp"),
+bmp(filename=paste(rep_name, "txt_contrib10.bmp"),
     width=2048, height=2048, res=200)
 plot_obj <- fviz_ca_col(AFC, select.col=list(contrib=10),col.var="contrib", 
     gradient.cols=c("#ffc2ca", "#ff0000"), shape.col=19, repel=T)
 	print(plot_obj)
 dev.off()
 
-bmp(filename=paste(var_name, "motif_contrib10.bmp"),
+bmp(filename=paste(rep_name, "motif_contrib10.bmp"),
     width=2048, height=2048, res=200)
 plot_obj <- fviz_ca_row(AFC, select.row=list(contrib=10),col.var="contrib", gradient.rows=c("#ffc2ca", "#ff0000"), shape.col=19, repel=T)
     print(plot_obj)
 dev.off()
   
-bmp(filename=paste(var_name, "txt_contrib10_size.bmp"), width=2048, height=2048, res=200)
+bmp(filename=paste(rep_name, "txt_contrib10_size.bmp"), width=2048, height=2048, res=200)
 plot_obj <- fviz_ca_col(AFC, select.col=list(contrib=10),pointsize="contrib", shape.col=19, repel=T)
 print(plot_obj)
 dev.off()
 
-bmp(filename=paste(var_name, "txt_contrib20_size.bmp"), width=2048, height=2048, res=200)
+bmp(filename=paste(rep_name, "txt_contrib20_size.bmp"), width=2048, height=2048, res=200)
 plot_obj <- fviz_ca_col(AFC, select.col=list(contrib=20),pointsize="contrib", shape.col=19, repel=T)
 print(plot_obj)
 dev.off()
 
-bmp(filename=paste(var_name, "motif_contrib10_size.bmp"), width=2048, height=2048, res=200)
+bmp(filename=paste(rep_name, "motif_contrib10_size.bmp"), width=2048, height=2048, res=200)
 plot_obj <- fviz_ca_row(AFC, select.row=list(contrib=10),pointsize="contrib", shape.row=19, repel=T)
 print(plot_obj)
 dev.off()
 
-bmp(filename=paste(var_name, "motif_contrib20_size.bmp"), width=2048, height=2048, res=200)
+bmp(filename=paste(rep_name, "motif_contrib20_size.bmp"), width=2048, height=2048, res=200)
 plot_obj <- fviz_ca_row(AFC, select.row=list(contrib=20),pointsize="contrib", shape.col=19, repel=T)
 print(plot_obj)
 dev.off()
 
-bmp(filename=paste(var_name, "motif_contrib30_size.bmp"), width=2048, height=2048, res=200)
+bmp(filename=paste(rep_name, "motif_contrib30_size.bmp"), width=2048, height=2048, res=200)
 plot_obj <- fviz_ca_row(AFC, select.row=list(contrib=30),pointsize="contrib", shape.row=19, repel=T)
 print(plot_obj)
 dev.off()
 
-bmp(filename=paste(var_name, "text_contrib30_size.bmp"), width=2048, height=2048, res=200)
+bmp(filename=paste(rep_name, "text_contrib30_size.bmp"), width=2048, height=2048, res=200)
 plot_obj <- fviz_ca_col(AFC, select.col=list(contrib=30),pointsize="contrib", shape.col=19, repel=T)
 print(plot_obj)
 dev.off()
@@ -218,30 +221,30 @@ dev.off()
 #------hierarchical clustering------#
 hclust = HCPC(AFC, nb.clust=-1, graph=F)
 
-bmp(filename=paste(var_name, "hclust_map.bmp"), width=2048, height=2048, res=200)
+bmp(filename=paste(rep_name, "hclust_map.bmp"), width=2048, height=2048, res=200)
 plot.HCPC(hclust, choice="map")
 dev.off()
 
-bmp(filename=paste(var_name, "hclust_map_light.bmp"), width=2048, height=2048, res=200)
+bmp(filename=paste(rep_name, "hclust_map_light.bmp"), width=2048, height=2048, res=200)
 plot(hclust, choice="map", ind.names = FALSE)
 dev.off()
 
-bmp(filename=paste(var_name, "hclust_chute.bmp"), width=2048, height=2048, res=200)
+bmp(filename=paste(rep_name, "hclust_chute.bmp"), width=2048, height=2048, res=200)
 plot.HCPC(hclust, choice="bar")
 dev.off()
 
-capture.output(hclust$desc.var, file=paste(var_name, "hclust_desc_var.txt"))
-capture.output(hclust$desc.ind, file=paste(var_name, "hclust_desc_ind.txt"))
+capture.output(hclust$desc.var, file=paste(rep_name, "hclust_desc_var.txt"))
+capture.output(hclust$desc.ind, file=paste(rep_name, "hclust_desc_ind.txt"))
 
 
 #----IMPORTANT ! création du rep "motifs_cluster" dans lequel le script interpret_association_motif.py vient chercher ses motifs----#
 
-dir.create(glue("./Patterns_results/R/{rep_name}/motifs_cluster"))
+dir.create(glue("{rep_name}motifs_cluster"))
 
 for (cluster in 1:hclust$call$t$nb.clust) {
-	para = glue("./Patterns_results/R/{rep_name}/motifs_cluster/{cluster}_para.csv")
+	para = glue("{rep_name}motifs_cluster/{cluster}_para.csv")
 	write.csv(hclust$desc.ind$para[cluster], file=para, row.names=TRUE)
-	dist = glue("./Patterns_results/R/{rep_name}/motifs_cluster/{cluster}_dist.csv")
+	dist = glue("{rep_name}motifs_cluster/{cluster}_dist.csv")
 	write.csv(hclust$desc.ind$dist[cluster], file=dist, row.names=TRUE)
 }
 
@@ -249,19 +252,19 @@ for (cluster in 1:hclust$call$t$nb.clust) {
 
 #--force 2 clusters : optionnal--#
 hclust_force2 = HCPC(AFC, nb.clust=2, graph=F)
-bmp(filename=paste(var_name, "hclust_force2_map.bmp"), width=2048, height=2048, res=200)
+bmp(filename=paste(rep_name, "hclust_force2_map.bmp"), width=2048, height=2048, res=200)
 plot.HCPC(hclust_force2, choice="map")
 dev.off()
-bmp(filename=paste(var_name, "hclust_force2_chute.bmp"), width=2048, height=2048, res=200)
+bmp(filename=paste(rep_name, "hclust_force2_chute.bmp"), width=2048, height=2048, res=200)
 plot.HCPC(hclust_force2, choice="bar")
 dev.off()
-capture.output(hclust_force2$desc.var, file=paste(var_name, "hclust_force2_desc_var.txt"))
-capture.output(hclust_force2$desc.ind, file=paste(var_name, "hclust_force2_desc_ind.txt"))
+capture.output(hclust_force2$desc.var, file=paste(rep_name, "hclust_force2_desc_var.txt"))
+capture.output(hclust_force2$desc.ind, file=paste(rep_name, "hclust_force2_desc_ind.txt"))
 
 
 
 
-} #ferme l'accolade ouverte ligne 26
+ #ferme l'accolade ouverte ligne 26
 
 
 
