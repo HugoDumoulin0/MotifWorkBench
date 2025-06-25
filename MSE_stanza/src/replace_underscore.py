a1a4838f41c9d4d2b66d6d673b97b74a06c900c8
Your Name <hugo.dumoulin@hotmail.fr>
2025-06-25 17:32:33 +0200

    tim fix for "du"

--- MSE_stanza/src/replace_underscore.py
+++ MSE_stanza/src/replace_underscore.py
@@ -5,10 +5,10 @@ import random
 def replace_underscore_in_conllu(file_path):
     """
     ConllU tables include '_' for N/A properties. This is problematic for pattern mining.
-    This replaces '_' by a recopy of the form of the word, introducing redundancy and (I
-    hope) protecting patterns for over-generation.
+    This replaces '_' by a random integer.
 
-    Replace any cell containing '_' with the content of the second cell in the same row.
+    ConllU tables also include 'du' repeated for all columns for contraction (de+le, etc.).
+    This deletes their lines.
     
     Args:
     file_path (str): The path to the .conllu file.
@@ -16,38 +16,19 @@ def replace_underscore_in_conllu(file_path):
     # Read the file with tab separator, skipping comment lines
     df = pd.read_csv(file_path, sep='\t', comment='#', header=None, quoting=3, engine='python')
     
-    # Process each row to replace '_'
+    # Remove rows where first column is a digit range (e.g., '3-4')
+    df = df[~df[0].astype(str).str.match(r'^\d+-\d+$')]
+
+    # Process each row to replace '_' and 'du'
     for index, row in df.iterrows():
         random_integer = random.randint(1, 100)
-        # second_cell_value = row[1]  # Get the value of the second cell in the row
-        # if row[2] == "_":
-        #     row[2]=f"underscorefix{random_integer}"
-        # if row[3] == "_":
-        #     row[3]=f"underscorefix{random_integer}"
-        # if row[4] == "_":
-        #     row[4]=random_integer
-        # if row[5] == "_":
-        #     mod = f"underscore_fix={random_integer}"
-        #     row[5] = mod
-        # if row[6]=="_":
-        #     row[6]=random_integer
-        # if row[7]=="_":
-        #     row[7]=f"underscorefix{random_integer}"
-        # if row[8] == "_":
-        #     row[8] = f"{random_integer}:{random_integer}"
-        # if row[9]=="_":
-        #     row[9]=f"underscorefix{random_integer}"
         df.loc[index] = row.apply(lambda x: random_integer if x == '_' else x)
+        # df.loc[index] = row.apply(lambda x: random_integer if x == 'du' else x)
+        # df.loc[index] = row.apply(lambda x: random_integer if x == 'des' else x)
+        # df.loc[index] = row.apply(lambda x: random_integer if x == 'au' else x)
+        # df.loc[index] = row.apply(lambda x: random_integer if x == 'aux' else x)
+        # This was a temp fix (Tim, 2025-06-04): it would be better to ignore lines with 'du' in col 2 or with two integers in col 1.
+
     # Write back to the same file
     df.to_csv(file_path, sep='\t', index=False, header=False, quoting=3)
-
-# def delete_empty_line(file_path):
-#     # Read the file with tab separator, skipping comment lines
-#     df = pd.read_csv(file_path, sep='\t', comment='#', header=None, quoting=3, engine='python')
-
-#     # Remove rows where the entire row contains only numbers and tabs
-#     df_cleaned = df[~df.apply(lambda x: x.str.match(r'^[0-9\t]*$').all(), axis=1)]
-
-#     # Save the cleaned DataFrame back to a tab-separated file
-#     df_cleaned.to_csv(file_path, sep='\t', index=False, header=False)
 
