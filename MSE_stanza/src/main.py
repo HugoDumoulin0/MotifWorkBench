d0be60117ee8d129652c95a7f1c0bc78e5b68501
Your Name <hugo.dumoulin@hotmail.fr>
2025-07-16 21:24:37 +0200

    update

--- MSE_stanza/src/main.py
+++ MSE_stanza/src/main.py
@@ -388,21 +388,21 @@ if __name__ == "__main__":
                  clustering_index_1 = regroupement.regroupement_1(index_motifs)
                  print("\t Nbr clusters : ", len(clustering_index_1))
                  
-                 title_file_results_1 = "./Clustering_results/Clusters/clustering_1.pk"
-                 if not os.path.exists(title_file_results_1):
-                     clustering_motifcodes_1 = regroupement.from_index_to_motifcodes(clustering_index_1, index_motifs)
-                     regroupement.save_pickles_results(clustering_motifcodes_1, title_file_results_1)
-                     # regroupement.save_pickles_results(clustering_motifcodes_1, title_file_results_1.replace(".pk", ".txt"))
-                     tools.save_as_txt(clustering_motifcodes_1, title_file_results_1.replace(".pk", ".txt"))
+                 title_file_results_1 = f"./Clustering_results/Clusters/{nb_itemset_min}_{minsup_percent}_{gap_min}{gap_max}_clustering_1.pk"
+                 # if not os.path.exists(title_file_results_1):
+                 clustering_motifcodes_1 = regroupement.from_index_to_motifcodes(clustering_index_1, index_motifs)
+                 regroupement.save_pickles_results(clustering_motifcodes_1, title_file_results_1)
+                 # regroupement.save_pickles_results(clustering_motifcodes_1, title_file_results_1.replace(".pk", ".txt"))
+                 tools.save_as_txt(clustering_motifcodes_1, title_file_results_1.replace(".pk", ".txt"))
              
                  print("5.2. Centroids 1 : 2/6")
              
-                 title_file_out_centroids_1 = "./Clustering_results/Medoids/medoids_1.pk"
-                 if not os.path.exists(title_file_out_centroids_1):
-                     compute_all_centroids = regroupement.main_compute_medoids(title_file_results_1, nbr_pool)
-                     regroupement.save_pickles_results(compute_all_centroids, title_file_out_centroids_1)
-                     # regroupement.save_pickles_results(compute_all_centroids, title_file_out_centroids_1.replace(".pk", ".txt"))
-                     tools.save_as_txt(compute_all_centroids, title_file_out_centroids_1.replace(".pk", ".txt"))
+                 title_file_out_centroids_1 = f"./Clustering_results/Medoids/{nb_itemset_min}_{minsup_percent}_{gap_min}{gap_max}_medoids_1.pk"
+                # if not os.path.exists(title_file_out_centroids_1):
+                 compute_all_centroids = regroupement.main_compute_medoids(title_file_results_1, nbr_pool)
+                 regroupement.save_pickles_results(compute_all_centroids, title_file_out_centroids_1)
+                 # regroupement.save_pickles_results(compute_all_centroids, title_file_out_centroids_1.replace(".pk", ".txt"))
+                 tools.save_as_txt(compute_all_centroids, title_file_out_centroids_1.replace(".pk", ".txt"))
 
                  print("5.3. Clustering 2 : 3/6")
              
@@ -412,22 +412,22 @@ if __name__ == "__main__":
              
                  index_clusters = list(clusters_1.keys())
                  
-                 title_file_results_2 = "./Clustering_results/Clusters/clustering_2.pk"
-                 if not os.path.exists(title_file_results_2):
-                     clusters_2 = regroupement.clustering_2(index_clusters, clusters_1, centroids_files, nbr_pool)
-                     regroupement.save_pickles_results(clusters_2, title_file_results_2)
-                     # regroupement.save_pickles_results(clusters_2, title_file_results_2.replace(".pk", ".txt"))
-                     tools.save_as_txt(clusters_2, title_file_results_2.replace(".pk", ".txt"))
+                 title_file_results_2 = f"./Clustering_results/Clusters/{nb_itemset_min}_{minsup_percent}_{gap_min}{gap_max}_clustering_2.pk"
+                 # if not os.path.exists(title_file_results_2):
+                 clusters_2 = regroupement.clustering_2(index_clusters, clusters_1, centroids_files, nbr_pool)
+                 regroupement.save_pickles_results(clusters_2, title_file_results_2)
+                 # regroupement.save_pickles_results(clusters_2, title_file_results_2.replace(".pk", ".txt"))
+                 tools.save_as_txt(clusters_2, title_file_results_2.replace(".pk", ".txt"))
 
              
                  print("5.4. Centroids 2 : 4/6")
              
-                 title_file_out_centroids_2 = "./Clustering_results/Medoids/medoids_2.pk"
-                 if not os.path.exists(title_file_out_centroids_2):
-                     compute_all_centroids_2 = regroupement.main_compute_medoids(title_file_results_2, nbr_pool)
-                     regroupement.save_pickles_results(compute_all_centroids_2, title_file_out_centroids_2)
-                     # regroupement.save_pickles_results(compute_all_centroids_2, title_file_out_centroids_2.replace(".pk", ".txt"))
-                     tools.save_as_txt(compute_all_centroids_2, title_file_out_centroids_2.replace(".pk", ".txt"))
+                 title_file_out_centroids_2 = f"./Clustering_results/Medoids/{nb_itemset_min}_{minsup_percent}_{gap_min}{gap_max}_medoids_2.pk"
+                 # if not os.path.exists(title_file_out_centroids_2):
+                 compute_all_centroids_2 = regroupement.main_compute_medoids(title_file_results_2, nbr_pool)
+                 regroupement.save_pickles_results(compute_all_centroids_2, title_file_out_centroids_2)
+                 # regroupement.save_pickles_results(compute_all_centroids_2, title_file_out_centroids_2.replace(".pk", ".txt"))
+                 tools.save_as_txt(compute_all_centroids_2, title_file_out_centroids_2.replace(".pk", ".txt"))
 
 
                  print("5.5. Clustering 3 : 5/6")
@@ -437,23 +437,23 @@ if __name__ == "__main__":
              
                  print("\t Nbr clusters : ", len(centroids_files_2))
              
-                 title_file_results_3 = "./Clustering_results/Clusters/clustering_3.pk"
-                 if not os.path.exists(title_file_results_3):
-                     index_clusters = list(clusters_2.keys())
-                     clusters_3 = regroupement.clustering_2(index_clusters, clusters_2, centroids_files_2, nbr_pool)
-                     regroupement.save_pickles_results(clusters_3, title_file_results_3)
-                     # regroupement.save_pickles_results(clusters_3, title_file_results_3.replace(".pk",".txt"))
-                     tools.save_as_txt(clusters_3, title_file_results_3.replace(".pk",".txt"))
+                 title_file_results_3 = f"./Clustering_results/Clusters/{nb_itemset_min}_{minsup_percent}_{gap_min}{gap_max}_clustering_3.pk"
+                 # if not os.path.exists(title_file_results_3):
+                 index_clusters = list(clusters_2.keys())
+                 clusters_3 = regroupement.clustering_2(index_clusters, clusters_2, centroids_files_2, nbr_pool)
+                 regroupement.save_pickles_results(clusters_3, title_file_results_3)
+                 # regroupement.save_pickles_results(clusters_3, title_file_results_3.replace(".pk",".txt"))
+                 tools.save_as_txt(clusters_3, title_file_results_3.replace(".pk",".txt"))
 
              
                  print("5.6. Centroids 3 : 6/6")
                  
-                 title_file_out_centroids_3 = "./Clustering_results/Medoids/medoids_3.pk"
-                 if not os.path.exists(title_file_out_centroids_3):
-                     compute_all_centroids_3 = regroupement.main_compute_medoids(title_file_results_3, nbr_pool)
-                     regroupement.save_pickles_results(compute_all_centroids_3, title_file_out_centroids_3)
-                     # regroupement.save_pickles_results(compute_all_centroids_3, title_file_out_centroids_3.replace(".pk", ".txt"))
-                     tools.save_as_txt(compute_all_centroids_3, title_file_out_centroids_3.replace(".pk", ".txt"))
+                 title_file_out_centroids_3 = f"./Clustering_results/Medoids/{nb_itemset_min}_{minsup_percent}_{gap_min}{gap_max}_medoids_3.pk"
+                 # if not os.path.exists(title_file_out_centroids_3):
+                 compute_all_centroids_3 = regroupement.main_compute_medoids(title_file_results_3, nbr_pool)
+                 regroupement.save_pickles_results(compute_all_centroids_3, title_file_out_centroids_3)
+                 # regroupement.save_pickles_results(compute_all_centroids_3, title_file_out_centroids_3.replace(".pk", ".txt"))
+                 tools.save_as_txt(compute_all_centroids_3, title_file_out_centroids_3.replace(".pk", ".txt"))
 
         if internal_clustering==True:
             if not os.path.exists("./Clustering_results"):
