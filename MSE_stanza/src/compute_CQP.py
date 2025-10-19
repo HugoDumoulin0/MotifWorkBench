1ecbd5faa9be7ed00439ac55f01ea3bfd72c146e
Your Name <hugo.dumoulin@hotmail.fr>
2025-10-19 20:27:37 +0200

    bug fix when path_id == path_out

--- MSE_stanza/src/compute_CQP.py
+++ MSE_stanza/src/compute_CQP.py
@@ -325,21 +325,22 @@ def main(types_textes, minsup_percent,gap_min, gap_max, nb_itemset_min, specifs,
             
             #shortcut pour récupérer un df_k déjà produit par précédente instanciation du script avec la metadata "id"
             path_id = f"./Patterns_results/R/id/itemset_min{nb_itemset_min}/gap_min{gap_min}/gap_max{gap_max}/minsup{str(minsup_percent)}/"
-            if os.path.exists(path_id):
-                print("re-using computing data from 'id' metadata instanciation of script")
-                for dir in os.listdir(path_id):
-                    fichiers = sorted(os.listdir(path_id+dir), key=lambda f: os.path.getmtime(os.path.join(path_id+dir, f)),reverse=True)
-                    for f in fichiers: 
-                            if "motifsTexte_" in f:
-                                print("re-using : " + f)
-                                file_id=path_id+dir+"/"+f
-                                df_k=pd.read_csv(file_id, sep="\t", index_col=0)
-                                break
-                    file_out_motifs=path_out+dir+"/"+f
-                    chaine=path_out+dir+"/"+f
-                    file_total=chaine.replace("motifsTexte","motifsTexteOrdered",1)
-                    path_out=path_out+dir+"/"
-                    os.mkdir(path_out)
+            if path_id!=path_out:
+                if os.path.exists(path_id):
+                    print("re-using computing data from 'id' metadata instanciation of script")
+                    for dir in os.listdir(path_id):
+                        fichiers = sorted(os.listdir(path_id+dir), key=lambda f: os.path.getmtime(os.path.join(path_id+dir, f)),reverse=True)
+                        for f in fichiers: 
+                                if "motifsTexte_" in f:
+                                    print("re-using : " + f)
+                                    file_id=path_id+dir+"/"+f
+                                    df_k=pd.read_csv(file_id, sep="\t", index_col=0)
+                                    break
+                        file_out_motifs=path_out+dir+"/"+f
+                        chaine=path_out+dir+"/"+f
+                        file_total=chaine.replace("motifsTexte","motifsTexteOrdered",1)
+                        path_out=path_out+dir+"/"
+                        os.mkdir(path_out)
 
             else:
                 print("computing from scratch")
