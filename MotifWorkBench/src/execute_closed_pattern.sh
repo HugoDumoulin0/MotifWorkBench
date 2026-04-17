eebd9ef2acac7c237f1f4a8c09a910169dc3d2dc
Timothee <timothee.premat@gmail.com>
2026-04-17 18:02:11 +0200

    Revert "Update of execute_close_patterns.sh to avoid Docker on Linux; TODO: use Docker for Windows"
    This reverts commit 5f358b790e4386a63cbfb035be1c3a73ff12b7a2.

--- MotifWorkBench/src/execute_closed_pattern.sh
+++ MotifWorkBench/src/execute_closed_pattern.sh
@@ -1,22 +1,13 @@
 #!/usr/bin/env bash
 set -e
-os="$(uname)"
+
 BASE_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 
-if [ "$os" = "Darwin" ];then
-  docker run --rm --platform linux/amd64 \
-    -v "$BASE_DIR:/data" \
-    ubuntu:22.04 \
-    bash -c "
-      cd /data/BideSpanTree/bin &&
-      chmod +x bidespantree &&
-      ./bidespantree > /data/Patterns_results/Closed/$1
-    "
-    echo "Running BideSpanTree in a Docker container"
-else
-  echo "Running BideSpanTree natively";
-      cd "./BideSpanTree/bin";
-      ./bidespantree > "../../Patterns_results/Closed/$1";
-  echo "$(ls)";
-      cd ../..;
-fi
+docker run --rm --platform linux/amd64 \
+  -v "$BASE_DIR:/data" \
+  ubuntu:22.04 \
+  bash -c "
+    cd /data/BideSpanTree/bin &&
+    chmod +x bidespantree &&
+    ./bidespantree > /data/Patterns_results/Closed/$1
+  "
